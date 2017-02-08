a2171278e19e4a6561912f6cd7405e3c7f9ccc2b
softech86 <softech86@163.com>
2017-02-08 17:52:28 +0800

    1.init

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # vue-web
+
+To start:
+
+```bash
+$ npm install
+```
+
+To develop:
+
+```bash
+$ npm run dev
+```
+
+To build for production:
+
+```bash
+$ npm run build
+```
+
+To lint you code:
+
+```bash
+$ npm run lint
+```
+
---
+++ client/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="page">
+    <navbar></navbar>
+    <slide></slide>
+  </div>
+</template>
+
+<script>
+  import Counter from 'components/Counter'
+
+  export default {
+      components: {
+        Counter
+      }
+  }
+</script>
+
+<style lang="scss">
+  p {
+    color: red;
+  }
+</style>
